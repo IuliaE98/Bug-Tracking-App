44dda1ed86dbb11c91fa2fffaa9d83fb6341593d
Iulia Racovita <racovitaiuliaelena@gmail.com>
2019-12-19 23:36:45 +0000

    connected the client to the server

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,37 @@
 import React from 'react';
-import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      projects: []
+    };
+  }
+  
+  componentDidMount = () => {
+    axios.get('http://18.223.122.208:8080/api/project').then(projects => {
+      this.setState({
+        projects: projects.data
+      })
+    })
+  }
+  
+  render() {
+    const projects = this.state.projects.map((project, index) => <div key={index}>{project.name}</div>)
+    return (
+      <div className="app">
+      <main>
+        <h1>Bughunter</h1>
+        <div className="projects">
+          <h2>Projects list</h2>
+          <div>{projects}</div>
+        </div>
+      </main>
     </div>
-  );
+    );
+  }
 }
 
 export default App;
